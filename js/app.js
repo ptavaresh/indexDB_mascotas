f19313d589d870616dc0e375756c3eacb96d5ee7
Pedro Tavares <ptavaresh@gmail.com>
2019-04-11 22:45:28 -0500

    creating the project

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let DB;
+
+//seleccion de la interfaz
+const form = document.querySelector('form'),
+      nombreMascota = document.querySelector('#mascota'),
+      nombreCliente = document.querySelector('#cliente'),
+      telefono = document.querySelector('#telefono'),
+      fecha = document.querySelector('#fecha'),
+      sintomas = document.querySelector('#sintomas'),
+      citas = document.querySelector('#citas'),
+      headingAdministra = document.querySelector('#administra');
